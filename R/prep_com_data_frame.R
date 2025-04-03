053e34d959330fb6b93d5cb83a2b10e1958957d2
Darren Henderson <darrenh@ccsj.edu>
2025-04-02 22:55:16 -0500

    Upgraded to testthat 3. Added helper functions to prep-com-dataframe to improve readability. Created test for prep-com-dataframe to demonstate improved self-containment.

--- R/prep_com_data_frame.R
+++ R/prep_com_data_frame.R
@@ -16,50 +16,99 @@ prep_com_data_frame <- function(df) {
 
   colnames(df) <- stringr::str_to_upper(colnames(df))
 
-  #cips could be 6-digit characters: if so, add the period
-  if(sum(grepl(df$MAJORCIP, pattern = "^[0-9]{6}$")) == nrow(df)){
+  # cips could be 6-digit characters: if so, add the period
+  if (sum(cip_code_is_valid(df$MAJORCIP)) == nrow(df)) {
     df <- df %>%
-      dplyr::mutate(MAJORCIP = gsub(pattern = "(^[0-9]{2})([0-9]{4}$)",
-                                    replacement = "\\1\\.\\2",
-                                    x = .data$MAJORCIP))
+      dplyr::mutate(MAJORCIP = add_period_to_cip_code(.data$MAJORCIP))
   }
 
-  #if cips end in .0000, the file read-in may lose the decimal and period
-  #need to add it back before we can proceed
-  df$MAJORCIP <- gsub(pattern = "(^[0-9]{1,2}$)", replacement = "\\1\\.0000", x = df$MAJORCIP)
+  # if cips end in .0000, the file read-in may lose the decimal and period
+  # need to add it back before we can proceed
+  df$MAJORCIP <- ensure_cip_code_has_period(df$MAJORCIP)
 
-  #now check that all cips have a period... if not, throw a warning
-  if(sum(!grepl(df$MAJORCIP, pattern = "\\.")) > 0){
+  # now check that all cips have a period... if not, throw a warning
+  if (cip_code_does_not_have_a_period(df$MAJORCIP)) {
     stop("Cip Codes are not in an accepted format. Review setup requirements for Completions")
   }
 
-  #if they do have a period, only proceed if the format isn't finished
+  # if they do have a period, only proceed if the format isn't finished
   df <- df %>%
-    tidyr::separate(col = .data$MAJORCIP,
-             into = c("Two", "Four"),
-             sep = "\\."
-    ) %>%
-    dplyr::mutate(Two = dplyr::case_when(
-      nchar(.data$Two) == 1 ~ paste0("0", .data$Two),
-      TRUE ~ .data$Two
-    ),
-    Four = dplyr::case_when(
-      nchar(.data$Four) == 0 ~ paste0(.data$Four, "0000"),
-      nchar(.data$Four) == 1 ~ paste0(.data$Four, "000"),
-      nchar(.data$Four) == 2 ~ paste0(.data$Four, "00"),
-      nchar(.data$Four) == 3 ~ paste0(.data$Four, "0"),
-      TRUE ~ .data$Four
-    ),
-    MAJORCIP = paste0(.data$Two, ".", .data$Four)
+    separate_cip_code_into_component_parts(.data$MAJORCIP) %>%
+    dplyr::mutate(Two = add_leading_zero_to_cip_lefthand_side(.data$Two),
+    Four = adjust_cip_righthand_side_format(.data$Four),
+    MAJORCIP = join_cip_components_with_period(.data$Two, .data$Four)
     ) %>%
     dplyr::select(-"Two", -"Four") %>%
-    dplyr::mutate(UNITID = as.character(.data$UNITID),
-                  DEGREELEVEL = as.character(.data$DEGREELEVEL))
+    dplyr::mutate(UNITID = ensure_data_is_character_type(.data$UNITID),
+                  DEGREELEVEL = ensure_data_is_character_type(.data$DEGREELEVEL))
 
   if("STUDENTID" %in% colnames(df)) {
     df <- df %>%
-      dplyr::mutate(STUDENTID = as.character(.data$STUDENTID))
+      dplyr::mutate(STUDENTID = ensure_data_is_character_type(.data$STUDENTID))
   }
 
   return(df)
+}
+
+# private functions
+
+# is_valid is a signal that the function returns a boolean: TRUE or FALSE
+# TRUE = 1
+# FALSE = 0
+# reason sum() == nrow(df) works, check all cases of the variable df$x
+# needs to match the number of rows in the dataframe.
+cip_code_is_valid <- function(x) {
+  grepl(x, pattern = "^[0-9]{6}$")
+}
+
+add_period_to_cip_code <- function(x) {
+  gsub(pattern = "(^[0-9]{2})([0-9]{4}$)",
+       replacement = "\\1\\.\\2",
+       x)
+}
+
+# use when cip code is in two parts separated by a period
+# and ends in four zero characters.
+ensure_cip_code_has_period <- function(x) {
+  gsub(pattern = "(^[0-9]{1,2}$)", replacement = "\\1\\.0000", x)
+}
+
+cip_code_has_a_period <- function(x) {
+  grepl(x, pattern = "\\.")
+}
+
+cip_code_does_not_have_a_period <- function(x) {
+  ! cip_code_has_a_period(x)
+}
+
+separate_cip_code_into_component_parts <- function(x) {
+  tidyr::separate(col = x,
+                  into = c("Two", "Four"),
+                  sep = "\\."
+  )
+}
+
+add_leading_zero_to_cip_lefthand_side <- function(x) {
+  dplyr::case_when(
+    nchar(x) == 1 ~ paste0("0", x),
+    TRUE ~ x
+  )
+}
+
+adjust_cip_righthand_side_format <- function(x) {
+   dplyr::case_when(
+     nchar(x) == 0 ~ paste0(x, "0000"),
+     nchar(x) == 1 ~ paste0(x, "000"),
+     nchar(x) == 2 ~ paste0(x, "00"),
+     nchar(x) == 3 ~ paste0(x, "0"),
+     TRUE ~ x
+   )
+}
+
+join_cip_components_with_period <- function(p1, p2) {
+  paste0(p1, ".", p2)
+}
+
+ensure_data_is_character_type <- function(x) {
+  as.character(x)
 }
---
+++ tests/testthat/test-prep_com_data_frame.R
@@ -0,0 +1,48 @@
+test_that("it converts dataframe column names to uppercase", {
+  # Arrange
+  # Act
+  # Assert
+
+  com_df <- data.frame(unitid = 123456,
+                       majorcip = c(2.34, 12.34, 2.3400, 2, 12),
+                       degreelevel = 17,
+                       studentid = 900)
+
+  df_actual <- prep_com_data_frame(com_df)
+  expect_equal(sum(grepl(colnames(df_actual), pattern = "[[:lower:]]")), 0)
+})
+
+test_that("it applies character data type conversions.", {
+
+  com_df <- data.frame(unitid = 123456,
+                       majorcip = c(2.34, 12.34, 2.3400, 2, 12),
+                       degreelevel = 17,
+                       studentid = 900)
+
+  expect_type(prep_com_data_frame(com_df)$UNITID, 'character')
+  expect_type(prep_com_data_frame(com_df)$MAJORCIP, 'character')
+  expect_type(prep_com_data_frame(com_df)$DEGREELEVEL, 'character')
+  expect_type(prep_com_data_frame(com_df)$STUDENTID, 'character')
+})
+
+test_that("it detects invalid cip code formats and throws an error", {
+
+  # not 1, 2, or 6 digits and no period in cip code
+  df_actual <- data.frame(unitid = 123456,
+                          degreelevel = 5,
+                          student_id = 100,
+                          majorcip = 345)
+
+  expect_error(prep_com_data_frame(df_actual))
+})
+
+test_that("it detects cip codes with mixed data types and throws an error", {
+
+  # mixing types (does it mean string instead of numeric or float?)
+  df_actual <- data.frame(unitid = 123456,
+                          degreelevel = 5,
+                          student_id = 100,
+                          majorcip = c("34.", "34.01", "340000"))
+
+  expect_error(prep_com_data_frame(df_actual))
+})
