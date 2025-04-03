adf336ee12f9a9af91a17ece71e20b828d27fb08
Darren Henderson <darrenh@ccsj.edu>
2025-04-03 18:23:45 -0500

    Added missing tests for prep_com_data_frame().

--- tests/testthat/test-prep_com_data_frame.R
+++ tests/testthat/test-prep_com_data_frame.R
@@ -46,3 +46,20 @@ test_that("it detects cip codes with mixed data types and throws an error", {
 
   expect_error(prep_com_data_frame(df_actual))
 })
+
+test_that("it handles CIP codes appropriately", {
+
+  com_df <- data.frame(unitid = 123456,
+                       majorcip = c(2.34, 12.34, 2.3400, 2, 12),
+                       degreelevel = 17,
+                       studentid = 900)
+
+  expect_equal(toString(prep_com_data_frame(com_df)$MAJORCIP), "02.3400, 12.3400, 02.3400, 02.0000, 12.0000")
+
+  com_df2 <- data.frame(unitid = 123456,
+                        majorcip = c("000000", "111111", "040000"),
+                        degreelevel = 7,
+                        studentid = 999)
+
+  expect_equal(toString(prep_com_data_frame(com_df2)$MAJORCIP), "00.0000, 11.1111, 04.0000")
+})
